6cb9eeac5f70b32071045779fae8fc4b7b461119
Samrat Dutta <samrat00725100@gmail.com>
2022-10-29 23:42:38 +0530

    Loader added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+        "body").style.visibility = "hidden";
+      document.querySelector(
+        "#loader").style.visibility = "visible";
+  } else {
+      document.querySelector(
+        "#loader").style.display = "none";
+      document.querySelector(
+        "body").style.visibility = "visible";
+  }
+};
+
+
+
 let images = [
   "url(../images/ban-1.jpg)",
   "url(../images/ban-2.jpg)",
